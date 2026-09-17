0f06c0b29ba4a8ea478d10f42033317751d3e086
Glen Bell <auto-test@scalr.lo>
2026-09-17 16:02:29 +0000

    Add SAP memory limit test configuration

--- main.tf
+++ main.tf
@@ -1,5 +1,23 @@
-resource "random_pet" "run_from_vcs" {
-  keepers = {
-    timestamp = timestamp()
+data "external" "test" {
+  program = ["bash", "-c", var.program_for_data_external]
+}
+
+variable "program_for_data_external" {
+  type    = string
+  default = "echo '{\"result\": \"test\"}'"
+}
+
+resource "terraform_data" "test" {
+  provisioner "local-exec" {
+    command = var.command_for_local_exec
   }
+}
+
+resource "terraform_data" "always_recreated" {
+  triggers_replace = timestamp()
+}
+
+variable "command_for_local_exec" {
+  type    = string
+  default = "echo 'ok'"
 }
